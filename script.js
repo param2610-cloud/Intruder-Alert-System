e07366618407e0ffb901b976f5ed9b868d288bc0
collegeparam <138299927+param2610-cloud@users.noreply.github.com>
2024-08-15 02:53:37 +0530

    dashboard done

---
+++ script.js
@@ -0,0 +1,27 @@
+async function populateTable() {
+    try {
+        const response = await fetch('http://localhost:3000/api/history');
+        console.log(response)
+        const data = await response.json();
+        const tbody = document.getElementById('data-table-tbody');
+
+        data.forEach(item => {
+            const tr = document.createElement('tr');
+            const timestamp = parseInt(item.time, 10);
+            const date = new Date(timestamp * 1000);
+            const formattedDate = date.toLocaleDateString();
+            const formattedTime = date.toLocaleTimeString();
+            tr.innerHTML = `
+                <td class="border px-4 py-2">${item._id}</td>
+                <td class="border px-4 py-2">${formattedDate || 'N/A'}</td>
+                <td class="border px-4 py-2">${formattedTime || 'N/A'}</td>
+                <td class="border px-4 py-2">${item.message || 'N/A'}</td>
+            `;
+            tbody.appendChild(tr);
+        });
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+window.onload = populateTable;
---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+const allowedOrigins = ['http://192.168.0.158:5500'];
+
+// CORS configuration
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+};
+
+// Enable CORS with specified options
+app.use(cors(corsOptions));
+
+// MongoDB connection URI
+const uri = 'mongodb+srv://shareallmedia69:LOdPb5Hm46gGhMv9@cluster.pa042.mongodb.net/?retryWrites=true&w=majority&appName=Cluster';
+const dbName = 'intruderHistory';
+const collectionName = 'History';
+
+app.get('/api/history', async (req, res) => {
+    const client = new MongoClient(uri);
+    
+    try {
+        console.log("hit")
+        await client.connect();
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        const documents = await collection.find({}).toArray();
+        res.json(documents); // Send data as JSON response
+    } catch (err) {
+        console.error('Error retrieving documents:', err);
+        res.status(500).send('Error retrieving documents');
+    } finally {
+        await client.close();
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
