b7d9895ccfd7a849a2255907d5cf918d2d3f1444
collegeparam <138299927+param2610-cloud@users.noreply.github.com>
2024-08-18 10:20:32 +0530

    ,,,

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 async function populateTable() {
     try {
-        const response = await fetch('https://intruder-alert-system.onrender.com/api/history');
-        console.log(response)
+        const response = await fetch(
+            "https://intruder-alert-system.onrender.com/api/history"
+        );
+        console.log(response);
         const data = await response.json();
-        const tbody = document.getElementById('data-table-tbody');
+        const tbody = document.getElementById("data-table-tbody");
 
-        data.forEach(item => {
-            const tr = document.createElement('tr');
-            const timestamp = parseInt(item.time, 10);
-            const date = new Date(timestamp * 1000);
-            const formattedDate = date.toLocaleDateString();
-            const formattedTime = date.toLocaleTimeString();
+        data.forEach((item) => {
+            const tr = document.createElement("tr");
+            const str = item.time
+            const date =str.split(",")[0] 
+            const time =str.split(",")[1] 
             tr.innerHTML = `
                 <td class="border px-4 py-2">${item._id}</td>
-                <td class="border px-4 py-2">${formattedDate || 'N/A'}</td>
-                <td class="border px-4 py-2">${formattedTime || 'N/A'}</td>
-                <td class="border px-4 py-2">${item.message || 'N/A'}</td>
+                <td class="border px-4 py-2">${date}</td>
+                <td class="border px-4 py-2">${time}</td>
+                <td class="border px-4 py-2">${item.message || "N/A"}</td>
             `;
             tbody.appendChild(tr);
         });
     } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
     }
 }
 
