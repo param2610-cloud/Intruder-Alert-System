342b5394642ecd3e613dfcf3654d49653584fd8e
The-Parthib <rijupanja81@gmail.com>
2024-09-03 21:40:21 +0530

    main code final

--- main2.py
+++ main2.py
@@ -1,31 +1,42 @@
 from machine import Pin
 import time
 import urequest as rq
+import internet
 
 
+stat = True
 
+led = Pin(9, Pin.OUT)
 led2 = Pin(2, Pin.OUT)
-
 buzzer = Pin(16, Pin.OUT)
 motion_sensor = Pin(15, Pin.IN)
+
 def alert_system(state):
-    
     led2.value(state)
-    
-    
     buzzer.value(state)
+
+def sys_on():
+    led.value(1)
+    time.sleep(0.7)
+    led.value(0)
+    time.sleep(0.7)
 
 try:
     while True:
         
         if motion_sensor.value() == 1:
             print("Motion detected!")
-            
-            rq.post("https://intruder-alert-system.onrender.com/api/intruder-alert")
+
+            if stat == True:
+                rq.post("https://intruder-alert-system.onrender.com/api/intruder-alert")
+                stat = False
+
             alert_system(1)  
             time.sleep(2)  
         else:
-            alert_system(0)  
+            alert_system(0)
+            sys_on()
+            stat = True
             
         time.sleep(0.1)  
         
